311755257d8be4beba22594466bb60d962976749
Charles <charlesw.manchester2019a@gmail.com>
2020-01-30 10:34:33 +0000

    toggle bar added

--- scripts.js
+++ scripts.js
@@ -5,3 +5,39 @@ function success() {
     'success'
   )
 }
+
+let censored = "true";
+
+function toggleSWF(){
+  if(document.getElementById('switch').checked){
+    censored = false;
+    document.getElementById('NSFW').style.color = "#ff3b3b"
+    document.getElementById('SFW').style.color = "#ffff"
+  } else{
+    censored = true;
+    document.getElementById('SFW').style.color = "#2196F3"
+    document.getElementById('NSFW').style.color = "#ffff"
+  }
+}
+
+function hoverEffect(){
+  if(censored){
+    document.getElementById('censorBar').style.display = "block";
+  } else{
+    document.getElementById('censorBar').style.display = "none";
+  }
+}
+function unhover(){
+  document.getElementById('censorBar').style.display = "none";
+}
+
+function hoverEffectP(){
+  if(censored){
+    document.getElementById('censorBarPhone').style.display = "block";
+  } else{
+    document.getElementById('censorBarPhone').style.display = "none";
+  }
+}
+function unhoverP(){
+  document.getElementById('censorBarPhone').style.display = "none";
+}
