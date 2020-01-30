827947326ae785b5bba3b66d142d8606fec344fa
Charles <charlesw.manchester2019a@gmail.com>
2020-01-30 12:13:07 +0000

    Changed image type

--- scripts.js
+++ scripts.js
@@ -8,37 +8,52 @@ function success() {
 
 let censored = "true";
 
-function toggleSWF(){
-  if(document.getElementById('switch').checked){
+function toggleSWF() {
+  if (document.getElementById('switch').checked) {
     censored = false;
     document.getElementById('NSFW').style.color = "#ff3b3b"
     document.getElementById('SFW').style.color = "#ffff"
-  } else{
+  } else {
     censored = true;
     document.getElementById('SFW').style.color = "#2196F3"
     document.getElementById('NSFW').style.color = "#ffff"
   }
 }
 
-function hoverEffect(){
-  if(censored){
+function hoverEffect() {
+  if (censored) {
     document.getElementById('censorBar').style.display = "block";
-  } else{
+  } else {
     document.getElementById('censorBar').style.display = "none";
   }
 }
-function unhover(){
+
+function unhover() {
   document.getElementById('censorBar').style.display = "none";
 }
 
-function hoverEffectP(){
-  if(censored){
-    document.getElementById('patrickImagePhone').src = "Images/patrickHoverSWF.webp";
-  } else{
-    document.getElementById('patrickImagePhone').src = "Images/patrickHover.webp";
-  }
-}
-function unhoverP(){
 
-  document.getElementById('patrickImagePhone').src = "Images/patrick.webp";
+
+function hoverEffectP() {
+  let patrick = document.getElementById('patrickImagePhone');
+  if (censored) {
+    patrick.src = "Images/patrickHoverSWF.webp";
+  } else {
+    patrick.src = "Images/patrickHover.webp";
+  }
+
+  patrick.style.width = "201px";
+  patrick.style.marginLeft = "-18%";
+  patrick.style.marginTop = "-17.5%";
+
+
+
+}
+
+function unhoverP() {
+  let patrick = document.getElementById('patrickImagePhone');
+
+  patrick.src = "Images/patrick.webp";
+  patrick.style.width = "100px";
+  patrick.style.margin = "0";
 }
